0ebf91d49b3fad46e0654a2050e6d146dc747961
Patrick Stalph <patrick.stalph@gmx.de>
2019-11-18 14:01:50 +0100

    Add unit test for ConverterDxfRect

---
+++ src/IxMilia.Converters.Test/ConverterPdfRectTests.cs
@@ -0,0 +1,32 @@
+﻿using IxMilia.Pdf;
+using Xunit;
+
+namespace IxMilia.Converters.Test
+{
+    public class ConverterPdfRectTests
+    {
+        [Theory]
+        [InlineData(16, 9)]
+        [InlineData(9, 16)]
+        public void AspectRatioIsKept(int maxWidth, int maxHeight)
+        {
+            ConverterDxfRect src = new ConverterDxfRect(0, 5, 0, 5);
+            PdfMeasurement pdfMaxWidth = PdfMeasurement.Points(maxWidth);
+            PdfMeasurement pdfMaxHeight = PdfMeasurement.Points(maxHeight);
+            ConverterPdfRect sut = ConverterPdfRect.KeepAspectRatio(src, pdfMaxWidth, pdfMaxHeight);
+
+            Assert.True(sut.Width.AsPoints() <= maxWidth);
+            Assert.True(sut.Height.AsPoints() <= maxHeight);
+
+            Assert.Equal(src.Width, src.Height); // otherwise test below is wrong
+            if (maxWidth > maxHeight)
+            {
+                Assert.Equal(pdfMaxHeight, sut.Height);
+            }
+            else
+            {
+                Assert.Equal(pdfMaxWidth, sut.Width);
+            }
+        }
+    }
+}
