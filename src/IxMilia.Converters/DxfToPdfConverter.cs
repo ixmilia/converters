eeed69faee59c7eb3aab1734fd8dab227d5d52a0
Patrick Stalph <patrick.stalph@gmx.de>
2019-11-18 14:04:45 +0100

    Convert style properties (color, stroke thickness) at least partially

--- src/IxMilia.Converters/DxfToPdfConverter.cs
+++ src/IxMilia.Converters/DxfToPdfConverter.cs
@@ -63,19 +63,18 @@ namespace IxMilia.Converters
             {
                 foreach (var entity in source.Entities.Where(e => e.Layer == layer.Name))
                 {
+                    var pdfStreamState = new PdfStreamState(
+                        strokeColor: GetPdfColor(entity, layer),
+                        strokeWidth: GetStrokeWidth(entity, layer));
                     switch (entity)
                     {
                         case DxfLine line:
-                            {
-                                var p1 = matrix.Transform(line.P1).ToPdfPoint(PdfMeasurementType.Point);
-                                var p2 = matrix.Transform(line.P2).ToPdfPoint(PdfMeasurementType.Point);
-                                AddPathItemToPage(
-                                    new PdfLine(p1, p2,
-                                        state: new PdfStreamState(
-                                            strokeColor: null, // TODO: apply color
-                                            strokeWidth: null))); // TODO: apply stroke
-                            }
+                        {
+                            var p1 = matrix.Transform(line.P1).ToPdfPoint(PdfMeasurementType.Point);
+                            var p2 = matrix.Transform(line.P2).ToPdfPoint(PdfMeasurementType.Point);
+                            AddPathItemToPage(new PdfLine(p1, p2, pdfStreamState));
                             break;
+                        }
                         default:
                             break;
                     }
@@ -102,8 +101,8 @@ namespace IxMilia.Converters
                 double dxfOffsetX = options.DxfSource.Left;
                 double dxfOffsetY = options.DxfSource.Bottom;
                 return Matrix4.CreateTranslate(+pdfOffsetX, +pdfOffsetY, 0.0)
-                     * Matrix4.CreateScale(scaleX, scaleY, 0.0)
-                     * Matrix4.CreateTranslate(-dxfOffsetX, -dxfOffsetY, 0.0);
+                    * Matrix4.CreateScale(scaleX, scaleY, 0.0)
+                    * Matrix4.CreateTranslate(-dxfOffsetX, -dxfOffsetY, 0.0);
             }
             // TODO this code assumes DXF unit inch - use actual unit from header instead!
             // scale depends on the unit, output "pdf points" with 72 DPI
@@ -113,5 +112,79 @@ namespace IxMilia.Converters
                 * Matrix4.CreateTranslate(-viewPort.LowerLeft.X, -viewPort.LowerLeft.Y, 0.0);
             return projectionMatrix;
         }
+        
+        #region Color and Stroke Width Conversion
+
+        private static PdfColor GetPdfColor(DxfEntity entity, DxfLayer layer)
+        {
+            int rgb = entity.Color24Bit;
+            if (rgb > 0)
+            {
+                return ToPdfColor(rgb);
+            }
+            DxfColor c = GetFinalDxfColor(entity, layer);
+            if (c != null && c.IsIndex)
+            {
+                rgb = c.ToRGB();
+                return ToPdfColor(rgb);
+            }
+            // default to black, probably not correct.
+            return new PdfColor(0, 0, 0);
+        }
+
+        private static PdfColor ToPdfColor(int rgb)
+        {
+            byte r = (byte)(rgb >> 16);
+            byte g = (byte)(rgb >> 8);
+            byte b = (byte)rgb;
+
+            // It seems DXF does not distinguish white/black:
+            // both map to index=7 which is (r=255,g=255,b=255)
+            // but white stroke on white background is crap.
+            // This doesn't feel right, better ideas?
+            if (r == byte.MaxValue && g == byte.MaxValue && b == byte.MaxValue)
+            {
+                return new PdfColor(0, 0, 0);
+            }
+            return new PdfColor(r / 255.0, g / 255.0, b / 255.0);
+        }
+
+        private static DxfColor GetFinalDxfColor(DxfEntity entity, DxfLayer layer)
+        {
+            DxfColor c = entity.Color;
+            if (c == null || c.IsByLayer)
+            {
+                return layer.Color;
+            }
+            if (c.IsIndex)
+            {
+                return c;
+            }
+            // we could build a Dictionary<DxfBlock, DxfColor> for the c.IsByBlock case
+            // not sure how to retrieve color for the remaining cases
+            return null;
+        }
+
+        private static PdfMeasurement GetStrokeWidth(DxfEntity entity, DxfLayer layer)
+        {
+            DxfLineWeight lw = new DxfLineWeight {Value = entity.LineweightEnumValue};
+            DxfLineWeightType type = lw.LineWeightType;
+            if (type == DxfLineWeightType.ByLayer)
+            {
+                lw = layer.LineWeight;
+            }
+            if (lw.Value == 0)
+            {
+                return default(PdfMeasurement);
+            }
+            if (lw.Value < 0)
+            {
+                return PdfMeasurement.Points(1); // smallest valid stroke width
+            }
+            // TODO What is the meaning of this short? Some default app-dependent table?
+            return PdfMeasurement.Points(lw.Value);
+        }
+
+        #endregion
     }
 }
